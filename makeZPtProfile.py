ef1481ddd82a4be2049e1d93ddda84ecae806e53
David Bjergaard <dbjergaard@gmail.com>
2014-06-13 11:31:26 -0400

    Functioning make file and script for building profile plots

--- makeZPtProfile.py
+++ makeZPtProfile.py
@@ -36,25 +36,30 @@ def writeCoords(histos, fName):
             datFile.write("%.5g\t%.7g\t%.7g\n"%(binWidth*(i+0.5), hist.mean(),yErr))
     datFile.write("#+END_PLOT\n")
     datFile.close()
+def getTitle(fileName):
+    titles={'1S0_8.yoda':'1S$_0^{(8)}$', '3PJ_8.yoda':'3P$_J^{(8)}$',
+            '3S1_8.yoda':'3S$_1^{(8)}$', '3PJ_1.yoda':'3P$_J^{(1)}$',
+            '3S1_1.yoda':'3S$_1^{(1)}$'}
+    title=fileName.split('.')[0]
+    try:
+        title=titles[fileName]
+    except KeyError:
+        pass
+    return title
 def printGnuPlot(outBName,plots):
     outFile=open(outBName+'.gnu','w')
     outFile.write('set term tikz standalone color solid size 5in,3in\n')
     outFile.write('set output \'%s.tex\'\n\n'%(outBName,))
     outFile.write('set key bottom left\n')
-    outFile.write('set key spacing 1.5\n')
+    outFile.write('set key spacing 2.25\n')
     outFile.write('set title "Charmed jet p$_{T}$ vs $\\\\langle z \\\\rangle$"\n')
     outFile.write('set ylabel "$\\\\langle z \\\\rangle$"\n')
     outFile.write('set xlabel "Jet $p_T$"\n')
     outFile.write('set xrange[0:%d]\n'%(nPtBins*binWidth))
     outFile.write('set yrange[0:1.10]\n')
-    # from colorbrew2.org, not color blind safe, not print safe, not copy safe 
-    colorWheel=['#92c5de','#4393c3','#2166ac',
-                '#b2182b','#d6604d','#f4a582',
-                '#a6cee3', '#1f78b4', '#b2df8a',
-                '#33a02c', '#fb9a99', '#e31a1c',
-                '#fdbf6f', '#ff7f00', '#cab2d6',
-                '#6a3d9a', '#ffff99', '#b15928']
-
+    # from colorbrew2.org, not color blind safe, not print safe, not
+    # copy safe, but they look good :)
+    colorWheel=['#b2182b','#d6604d', '#92c5de','#4393c3','#2166ac']
     lineStyle="set style line %d lc rgb '%s' lt 1 lw 2 pt 7 ps .75\n"
     # not idiomatic, but required, gnuplot must have linestyles
     # printed before the plot command
@@ -63,24 +68,20 @@ def printGnuPlot(outBName,plots):
     outFile.write('plot ')
     plotLine="'%s' \ttitle\t'%s' with \tyerrorbars ls %d"
     lPlots=[(k,plots[k]) for k in plots]
-    #plots=dict(lPlots[:-1])
     for i,plot in enumerate(lPlots[:-1]):
-        # print i,plot[0]
-        outFile.write(plotLine%(plot[0].split('.')[0]+'.txt',plot[0].split('.')[0],i+1)+',\\\n')
+        outFile.write(plotLine%(plot[0].split('.')[0]+'.txt',getTitle(plot[0]),i+1)+',\\\n')
     outFile.write(plotLine%(lPlots[-1][0].split('.')[0]+'.txt',
-                               lPlots[-1][0].split('.')[0],len(lPlots))+'\n')
+                            getTitle(lPlots[-1][0]),len(lPlots))+'\n')
     outFile.close()
 
 def main():
     if(len(args)==0):
         parser.print_help()
         return 1
-    print args
-    
-    # histos = getPtHistos(args)
-    # for yFile in histos:
-    #     writeCoords(histos[yFile],yFile)
-    # printGnuPlot('JetZvsPtProfile',histos)
+    histos = getPtHistos(args)
+    for yFile in histos:
+        writeCoords(histos[yFile],yFile)
+    printGnuPlot('JetZvsPtProfile',histos)
 
 if __name__ == '__main__':
     sys.exit(main())
